b4f0fbf13bf1e929d5640cf528b7f2904e13fcc9
biscake <leonut.ljh@gmail.com>
2024-08-03 13:10:14 +0800

    Fixed: separated products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,6 +1,7 @@
 import { useOutletContext } from "react-router-dom";
 import styles from "./Products.module.css";
 import { useProducts, useProductsHandler } from "./ProductsHelper";
+import { useState } from "react";
 
 const Products = () => {
   const data = useProducts();
@@ -11,28 +12,32 @@ const Products = () => {
 
   return (
     <div className={styles.wrapper}>
-      {data.map(product => {
-        const itemId = "item-" + product.id;
-        return (
-          <div key={product.id} className={styles.product}>
-            <img src={product.image} alt={product.title}/>
-            <h1>{product.title}</h1>
-            <p>{product.description}</p>
-            <p>${product.price}</p>
-            <p>{"Rating: " + product.rating.rate}</p>
-            <p>{"Count: " + product.rating.count}</p>
-            <label htmlFor={itemId}>Qty: </label>
-            <input type="number" name={itemId} id={itemId} min={0} max={99} value={0}/>
-            <button 
-              type="submit" 
-              onClick={(e) => {
-                e.preventDefault();
-                addToCart(product.id, document.querySelector(`#${itemId}`).value)
-              }}
-            >Add to cart</button>
-          </div>
-        )
-      })}
+      {data.map(product => <Product key={product.id} product={product} addToCart={addToCart} />)}
+    </div>
+  )
+}
+
+const Product = ({ product, addToCart }) => {
+  const [quantity, setQuantity] = useState(0);
+
+  const itemId = "item-" + product.id;
+  return (
+    <div key={product.id} className={styles.product}>
+      <img src={product.image} alt={product.title}/>
+      <h1>{product.title}</h1>
+      <p>{product.description}</p>
+      <p>${product.price}</p>
+      <p>{"Rating: " + product.rating.rate}</p>
+      <p>{"Count: " + product.rating.count}</p>
+      <label htmlFor={itemId}>Qty: </label>
+      <input type="number" name={itemId} id={itemId} min={0} max={99} value={quantity} onChange={(e) => setQuantity(e.target.value)}/>
+      <button 
+        type="submit" 
+        onClick={(e) => {
+          e.preventDefault();
+          addToCart(product.id, document.querySelector(`#${itemId}`).value)
+        }}
+      >Add to cart</button>
     </div>
   )
 }
--- tests/Products/products.test.jsx
+++ tests/Products/products.test.jsx
@@ -1,9 +1,9 @@
-import { describe, expect, it, test, vi } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, test, vi } from "vitest";
 import { Products } from "../../src/components/Products/Products";
 import { render, screen, waitFor  } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { useOutletContext } from "react-router-dom";
-import { useProductsHandler } from "../../src/components/Products/ProductsHelper";
+import { useProducts, useProductsHandler } from "../../src/components/Products/ProductsHelper";
 
 const mockProducts = [
   {
@@ -44,48 +44,59 @@ const mockProducts = [
 const mockAddToCart = vi.fn();
 
 
-vi.mock("../../src/components/Products/ProductsHelper", () => {
-  return {
-    useProducts: () => mockProducts,
-    useProductsHandler: () => {
-      return {
-        addToCart: mockAddToCart
-      }
-    }
-  };
-})
+vi.mock("../../src/components/Products/ProductsHelper");
+vi.mock("react-router-dom");
 
-vi.mock("react-router-dom", async (importOriginal) => {
-  return {
-      ...await importOriginal(),
-      useOutletContext: () => [{id:1, price: "5.0"}],
-    }
-})
+// vi.mock("react-router-dom", async (importOriginal) => {
+//   return {
+//       ...await importOriginal(),
+//       useOutletContext: () => [{id:1, price: "5.0"}],
+//     }
+// })
 
 
 describe("Products page", () => {
-  it("Should load products correctly", () => {
-    render(<Products/>);
-    mockProducts.forEach((product) => {
-      expect(screen.getByText(product.description)).toBeInTheDocument();
-      expect(screen.getByAltText(`Product ${product.id}`).src).toContain(`url${product.id}`);
-      expect(screen.getByText(`$${product.price}`)).toBeInTheDocument();
-      expect(screen.getByText("Rating: " + product.rating.rate)).toBeInTheDocument();
-      expect(screen.getByText("Count: " + product.rating.count)).toBeInTheDocument();
-    })
+  beforeEach(() => {
+    useProducts.mockReturnValue(mockProducts)
+    useOutletContext.mockReturnValue([])
   })
 
-  it("Add to cart button should be called when clicked", async () => {
-      const onClick = vi.fn();
-      const user = userEvent.setup();
-      render(<Products />);
+  afterEach(() => {
+    vi.clearAllMocks();
+    vi.resetAllMocks();
+  })
 
-      const buttons = screen.getAllByRole("button", { name: "Add to cart" });
+  describe("Should render correctly", () => {
+    beforeEach(() => {
+      useProductsHandler.mockReturnValue({ addToCart: mockAddToCart })
+    })
 
-      buttons.forEach(async (button) => {
-        await user.click(button);
+    it("Should load products correctly", () => {
+      render(<Products/>);
+      mockProducts.forEach((product) => {
+        expect(screen.getByText(product.description)).toBeInTheDocument();
+        expect(screen.getByAltText(`Product ${product.id}`).src).toContain(`url${product.id}`);
+        expect(screen.getByText(`$${product.price}`)).toBeInTheDocument();
+        expect(screen.getByText("Rating: " + product.rating.rate)).toBeInTheDocument();
+        expect(screen.getByText("Count: " + product.rating.count)).toBeInTheDocument();
       })
-      await waitFor(() => expect(mockAddToCart).toHaveBeenCalled(buttons.length));
-    }
-  )
+    })
+
+    it("Add to cart button should be called when clicked", async () => {
+        const user = userEvent.setup();
+        render(<Products />);
+
+        const buttons = screen.getAllByRole("button", { name: "Add to cart" });
+
+        buttons.forEach(async (button) => {
+          await user.click(button);
+        })
+        await waitFor(() => expect(mockAddToCart).toHaveBeenCalled(buttons.length));
+      }
+    )
+  })
+
+  describe("Check button functions correctly", () => {
+    
+  })
 })
